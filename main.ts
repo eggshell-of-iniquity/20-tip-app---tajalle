59bab96ea6869d03b46ee2aaa791fee211fee15d
eggshell-of-iniquity <82226129+eggshell-of-iniquity@users.noreply.github.com>
2021-04-23 19:15:34 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 let userPrice = game.askForNumber("How much are you paying?")
 let tipPrice = userPrice * 0.2
 let totalPrice = tipPrice + userPrice
-game.splash("You originally paid $" + userPrice + ", you're tipping $" + tipPrice + "\", and you're paying $" + totalPrice + " in total.")
+game.splash("You originally paid $" + userPrice + ", you're tipping $" + tipPrice + ", and you're paying $" + totalPrice + " in total.")
